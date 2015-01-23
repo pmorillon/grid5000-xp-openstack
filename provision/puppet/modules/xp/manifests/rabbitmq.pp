fc8a471b03db9484fb15351cf50d1747e6231cc8
Pascal Morillon <pascal.morillon@irisa.fr>
2015-01-23 11:38:36 +0100

    Configure rabbitmq server on controller

--- provision/puppet/modules/xp/manifests/rabbitmq.pp
+++ provision/puppet/modules/xp/manifests/rabbitmq.pp
@@ -5,4 +5,21 @@ class xp::rabbitmq {
       ensure => installed;
   }
 
+  service {
+    'rabbitmq-server':
+      ensure => running;
+  }
+
+  exec {
+    'change rabbitmq guest password':
+      command     => 'rabbitmqctl change_password guest rabbit',
+      path        => "/usr/bin:/usr/sbin:/bin",
+      user        => root,
+      group       => root,
+      refreshonly => true;
+  }
+
+  Package[rabbitmq-server] -> Service['rabbitmq-server']
+  Package[rabbitmq-server] ~> Exec['change rabbitmq guest password']
+
 }

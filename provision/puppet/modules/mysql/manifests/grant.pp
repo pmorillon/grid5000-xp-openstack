b9bf0a4dd53369c8cb6f253fa6d6a3a043df142d
Pascal Morillon <pascal.morillon@irisa.fr>
2015-01-26 11:21:50 +0100

    Fixes bug in Puppet module mysql

--- provision/puppet/modules/mysql/manifests/grant.pp
+++ provision/puppet/modules/mysql/manifests/grant.pp
@@ -8,7 +8,7 @@ define mysql::grant (
   exec {
     "Grant ${privileges} on ${database} for user ${user}":
       command => "/usr/bin/mysql --execute=\"GRANT ${privileges} ON ${database}.* TO '${user}'@'%' IDENTIFIED BY '${password}';\"",
-      unless  => "/usr/bin/mysql --database mysql --raw -NBe \"SHOW GRANTS FOR 'keystone'@'%';\"";
+      unless  => "/usr/bin/mysql --database mysql --raw -NBe \"SHOW GRANTS FOR '${user}'@'%';\"";
   }
 
   Package['mysql-server'] -> Exec <| |>
